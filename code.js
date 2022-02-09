3f2905bad56bdbacefc433e5c23f602ab9d40a8f
Chris Eliason <eliason.chris1@gmail.com>
2022-02-09 11:32:19 -0600

    unit tests for edge cases

--- code.js
+++ code.js
@@ -9,7 +9,12 @@ function helloWorld() {
 function sayHello(x) {
     if (x === "Pat" || x === "Alex" || x === "Jane") {
         return "Hello, " + x + "!"
-
+    }else if(typeof x === "boolean") {
+        return "Hello, World!"
+    }else if(typeof x === "object") {
+        return "Hello, World!"
+    }else if(typeof x === "function") {
+        return "Hello, World!"
     }else return "Hello, World!"
 }
 
